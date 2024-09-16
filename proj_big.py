685d1f81e04a2b307529b894929d4d0935b13bb9
dcollins <dcollins4096@gmail.com>
2024-09-16 11:41:43 -0400

    take 0

--- proj_big.py
+++ proj_big.py
@@ -3,9 +3,13 @@ from dtools.starter1 import *
 import tools.lite_grid as lgrid
 reload(lgrid)
 
-frame = 14
-hierarchy = '/scratch1/00369/tg456484/Paper83/Enzo/p83e_a05_Ms4.7_Ma1.5_2048/DD%04d/data%04d.hierarchy'%(frame,frame)
+frame = 57
+base = "/anvil/scratch/x-ux454321/p83_turbulence/Enzo/p83e_a04_Ms4.7_Ma1.5_1024"
+base = "/anvil/scratch/x-ux454321/p83_turbulence/Enzo/p83e_a03_Ms4.7_Ma1.5_512"
+base = "/anvil/scratch/x-ux454321/p83_turbulence/Enzo/p83e_b03_Ms4.7_Ma3_512"
+hierarchy = '%s/DD%04d/data%04d.hierarchy'%(base,frame,frame)
 field = 'Density'
 
-this_h = lgrid.lg(hierarchy)
+this_h = lgrid.fake_hierarchy(base,frame)
 this_h.parse()
+lgrid.proj(this_h,512,'Density',0,'%s/b03_n%04d_proj_Density'%(plot_dir,frame))
--- tools/lite_grid.py
+++ tools/lite_grid.py
@@ -1,27 +1,70 @@
 from dtools.starter1 import *
 
 class lg():
-    def __init__(self,fname):
-        self.fname=fname
-        self.grid_id=[]
-        self.left=[]
-        self.right=[]
-        self.left_zone=[]
-        self.right_zone=[]
+    def __init__(self,grid_id):
+        self.grid_id=grid_id
+
+class fake_hierarchy():
+    def __init__(self,base,frame):
+
+        self.base = base
+        self.frame=frame
+        self.fname= '%s/DD%04d/data%04d.hierarchy'%(base,frame,frame)
+        self.grids=[]
     def parse(self):
         fptr = open(self.fname,'r')
         lines = fptr.readlines()
         fptr.close()
         
         grid_id = None
+        this_grid=None
         for line in lines:
             san = line.strip()
             if len(san) == 0:
                 continue
             pair = line.split('=')
-            if pair[0]=='Grid':
+            key = pair[0].strip()
+            val = pair[1].strip()
+            if key=='Grid':
                 grid_id = int(pair[1])
-            print(grid_id)
+                this_grid = lg(grid_id)
+            elif key in ['GridLeftEdge','GridRightEdge']:
+                three = nar(val.split()).astype('float')
+                this_grid.__dict__[key] =three
+            elif key.startswith('BaryonFileName'):
+                this_grid.fname = val
+            elif key.startswith('Pointer') and this_grid is not None:
+                #the last entry
+                self.grids.append(this_grid)
+                this_grid=None
+
+def proj(this_h,top_grid_size,field,axis,fname):
+    N = top_grid_size
+    dim1 = [1,0,0][axis]
+    dim2 = [2,2,1][axis]
+    output = np.zeros([top_grid_size,top_grid_size])
+    for ng,ggg in enumerate(this_h.grids):
+        print('grid',ng)
+
+        left =  (ggg.GridLeftEdge*N).astype('int')
+        right = (ggg.GridRightEdge*N).astype('int')
+        h5ptr = h5py.File('%s/%s'%(this_h.base,ggg.fname),'r')
+        group = h5ptr['Grid%08d'%ggg.grid_id]
+        dset = group[field][()]
+        h5ptr.close()
+        proj = dset.sum(axis=axis)
+        output[left[dim1]:right[dim1],left[dim2]:right[dim2]] += proj
+    do_log=True
+    if do_log:
+        output = np.log10(output)
+    mpl.image.imsave(fname,output)
+
+
+
+
+
+
+                
 
 
 
